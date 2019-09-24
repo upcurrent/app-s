b3afe75d57102dda545a5786c083cee15698bdc6
aftershock <13046330784@163.com>
2019-09-24 14:57:18 +0800

    123

--- src/views/app_set/Interface.vue
+++ src/views/app_set/Interface.vue
@@ -17,23 +17,40 @@
                     </el-select>
                 </el-form-item>
                 <el-form-item>
-                    <el-button type="primary" style="margin-left: 10px">保存排序</el-button>
+                    <el-button type="primary" style="margin-left: 10px">保存</el-button>
                     <el-button>重置</el-button>
                 </el-form-item>
             </el-form>
 
 
             <el-table :data="interfaces" border style="width: 100%" height="680" >
-                <el-table-column type="selection" width="40" :selectable="handle_checkbox"></el-table-column>
-                <el-table-column align="center" prop="db" label="名称"></el-table-column>
+<!--                <el-table-column type="selection" width="40" :selectable="handle_checkbox"></el-table-column>-->
+                <el-table-column align="center" prop="db" label="原名称"></el-table-column>
+                <el-table-column align="center" prop="dbShow" label="显示名称">
+                    <i class="el-icon-edit"></i>
+                </el-table-column>
+                <el-table-column align="center" label="需要显示">
+
+                </el-table-column>
+                <el-table-column align="center" label="默认不清空"></el-table-column>
                 <el-table-column align="center" label="排序">
-                    <template >
+                    <template>
                         <el-button type="primary" plain icon="el-icon-upload2" circle></el-button>
                         <el-button icon="el-icon-top" circle></el-button>
                         <el-button icon="el-icon-bottom" circle></el-button>
                         <el-button type="primary" plain icon="el-icon-download" circle></el-button>
                     </template>
                 </el-table-column>
+<!--                <el-table-column-->
+<!--                        align="center"-->
+<!--                        label="操作"-->
+<!--                >-->
+<!--                    <template>-->
+<!--                        <el-button size="small">编辑</el-button>-->
+<!--                        <el-button type="danger" size="small">删除</el-button>-->
+<!--                    </template>-->
+<!--                </el-table-column>-->
+
             </el-table>
         </el-main>
     </el-container>
@@ -93,22 +110,30 @@
                 value: '',
 
                 interfaces: [{
-                    db: '单号'
+                    db: '单号',
+                    dbShow: '单据编码'
                 },{
-                    db: '单据日期'
+                    db: '单据日期',
+                    dbShow: '单据日期'
                 },{
-                    db: '供应商'
+                    db: '供应商',
+                    dbShow: '供应商'
                 },{
-                    db: '仓库'
+                    db: '仓库',
+                    dbShow: '仓库'
                 },{
-                    db: '经销方式'
+                    db: '经销方式',
+                    dbShow: '经销方式'
                 },{
-                    db: '业务员'
+                    db: '业务员',
+                    dbShow: '业务员'
                 },{
-                    db: '备注'
+                    db: '备注',
+                    dbShow: '备注'
                 },{
-                    db: '制单人'
-                }]
+                    db: '制单人',
+                    dbShow: '制单人'
+                },]
             }
         },
         methods:{
